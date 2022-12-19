1cd3ca0b12b1fb6c5000f1147ed5a5fad113ff15
Parth Panwar <71188742+Cocomekko@users.noreply.github.com>
2022-12-19 22:35:17 +0530

    Update app.js

--- coding-practice-3a/app.js
+++ coding-practice-3a/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const addDays = require("date-fns/addDays");
-const app = express();
-module.exports = app;
+const app = express(); 
+module.exports = app;   //exporting the instance of express
 app.get("/", (req, resp) => {
   const newDate = addDays(new Date(), 100);
   resp.send(`${newDate.getDate()}/${newDate.getMonth()+1}/${newDate.getFullYear()}`);
