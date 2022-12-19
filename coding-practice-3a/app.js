5ef291ed0c3e6e08c454ead4a8a3f473f06280ab
Cocomekko <parthpanwar2888@gmail.com>
2022-12-19 22:26:49 +0530

    practice

---
+++ coding-practice-3a/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+const app = express();
+module.exports = app;
+app.get("/", (req, resp) => {
+  const newDate = addDays(new Date(), 100);
+  resp.send(`${newDate.getDate()}/${newDate.getMonth()+1}/${newDate.getFullYear()}`);
+});
+app.listen(3000);
